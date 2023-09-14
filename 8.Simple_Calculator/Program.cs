41e3308eb0fa539df1b3c10429e2ce12d3fbf2a1
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-14 02:25:55 +0200

    Add project files.

---
+++ 8.Simple_Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter First Number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second number ");
+            double  num2 = Convert.ToDouble(Console.ReadLine());
+            double  num3 = num1 + num2;
+
+            Console.WriteLine(num1+ " + "+ num2 + " = " + num3 );
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 8.Simple_Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("8.Simple_Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("8.Simple_Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c77e9fd4-79ba-49e4-940c-577e65c38081")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
